022e251073146c9d95345f2b62f98e629387290c
Conte-Riesling <130985551+Conte-Riesling@users.noreply.github.com>
2024-03-12 16:55:37 +0200

    second commit

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager): 
+    def get_queryset(self):
+        return super().get_queryset()\
+    .filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -25,6 +31,10 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    
+    objects = models.Manager() # менеджер, применяемый по умолчанию 
+    published = PublishedManager() # конкретно-прикладной менеджер
+
 
     """Meta-класс определяет метаданные модели.
     Атрибут ordering, сообщающий Django, что он 
@@ -39,3 +49,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    
