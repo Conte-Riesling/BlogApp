81bd3b6f6ca71800276f14d3f848eefac913352c
Conte-Riesling <130985551+Conte-Riesling@users.noreply.github.com>
2024-03-19 16:43:09 +0200

    fourth commit

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager): 
@@ -18,7 +19,8 @@ class Post(models.Model):
     (черновик) Published (опубликован)"""
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
@@ -50,4 +52,10 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
-    
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.publish.slug])
